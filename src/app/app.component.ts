e9a8ec955966e55e54413bbd289855b886a1bbeb
Santiago-Zaurrini <santijz07@gmail.com>
2024-09-07 17:46:17 -0300

    Initial commit
    Table loaded & form initiated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { FileItem } from '../models/file.item.model';
-import { FILE_LIST } from '../data/file.storage';
+import { Component} from '@angular/core';
+import { FileItem, FileOwner, FileType } from '../models/file.item.model';
+import { FILE_LIST, OWNERS } from '../data/file.storage';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FormComponent } from "./form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  files: FileItem[] = FILE_LIST;
+  files: FileItem[] = FILE_LIST.sort((a, b) => {
+    if (a.type !== b.type) {
+      return a.type === FileType.FOLDER ? -1 : 1;
+    }
+    return a.name.localeCompare(b.name);
+  })
+  owners: FileOwner[] = OWNERS;
   title = 'file-management';
+
+  showForm: boolean = false;
+
+  onNewClick()
+  {
+    this.showForm = true;
+  }
 }
