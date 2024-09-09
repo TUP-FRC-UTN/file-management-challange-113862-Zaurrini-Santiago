ceffb5a129791b6b6f33f9f7517076362379991b
Santiago-Zaurrini <santijz07@gmail.com>
2024-09-09 20:33:39 -0300

    Next Commit
    Cancel & Save added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { FileItem, FileOwner, FileType } from '../models/file.item.model';
 import { FILE_LIST, OWNERS } from '../data/file.storage';
 import { CommonModule } from '@angular/common';
@@ -13,12 +13,12 @@ import { FormComponent } from "./form/form.component";
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  files: FileItem[] = FILE_LIST.sort((a, b) => {
+  files: (FileItem & { selected: boolean})[] = FILE_LIST.sort((a, b) => {
     if (a.type !== b.type) {
       return a.type === FileType.FOLDER ? -1 : 1;
     }
     return a.name.localeCompare(b.name);
-  })
+  }).map(file => ({ ...file, selected: false }));
   owners: FileOwner[] = OWNERS;
   title = 'file-management';
 
@@ -27,5 +27,42 @@ export class AppComponent {
   onNewClick()
   {
     this.showForm = true;
+  }
+
+  onFormClosed() {
+    this.showForm = false; 
+  }
+
+  saveForm(file: FileItem) {
+    this.files.push({ ...file, selected: false });
+    this.files.sort((a, b) => {
+      if (a.type !== b.type) {
+        return a.type === FileType.FOLDER ? -1 : 1;
+      }
+      return a.name.localeCompare(b.name);
+    });
+  }
+
+  deleteFiles()
+  {
+    const selectedFiles = this.files.filter(file => file.selected);
+
+    if(selectedFiles.length === 0){
+      alert('Please select at least one file to delete');
+      console.log('No file selected');
+      return;
+    }
+
+    if(selectedFiles.length > 1 ){
+      const confirmDelete = confirm('Seguro que quiere borrar?');
+
+      if(confirmDelete){
+        this.files = this.files.filter(file => !file.selected);
+        console.log('Files deleted', selectedFiles);
+      }
+    } else{
+      this.files = this.files.filter(file => !file.selected);
+      console.log('File deleted', selectedFiles);
+    }   
   }
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,48 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FileItem, FileOwner, FileType } from '../../models/file.item.model';
+import { OWNERS } from '../../data/file.storage';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent {
+export class FormComponent {  
+  owners: FileOwner[] = OWNERS;
+  selectedOwners: FileOwner[] = [];
 
+  filetype = FileType;
+  fileItem: FileItem = {
+    id: '',
+    name: '',
+    creation: new Date(),
+    owners: [{ name: '', avatarUrl: '' }],
+    type: FileType.FILE,
+    parentId: undefined,
+  };
+
+  @Output() formClosed = new EventEmitter<void>();
+  @Output() enviadoEmit = new EventEmitter<FileItem>();
+
+  save(form: NgForm) {
+    if (form.valid) {
+      console.log(this.fileItem.name, " ", this.fileItem.creation)
+      this.fileItem.id = Math.random().toString(36).substr(2, 9);
+      this.enviadoEmit.emit(this.fileItem);
+      this.formClosed.emit();
+      console.log(this.fileItem);
+    }
+    else{
+      alert('Llene los campos');
+      console.log(this.fileItem);
+    }
+  }
+
+  onClose() {
+    this.formClosed.emit(); // Emite el evento cuando se presiona "Volver"
+  }
 }
